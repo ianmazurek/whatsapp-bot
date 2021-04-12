b75c41627d6e923b6d41f67f0eb84e9c8a842bf7
ianmazurek <ianmazurek@protonmail.com>
2021-04-12 20:06:27 -0300

    Create msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -488,10 +488,7 @@ module.exports = msgHandler = async (client, message) => {
             await client.sendTextWithMentions(from, hehe)
             break
         case '!kickall':
-            if (!isGroupMsg) return client.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
             const isOwner = sender.id === chat.groupMetadata.owner
-            if (!isOwner) return client.reply(from, 'Perintah ini hanya bisa di gunakan oleh Owner group', id)
-            if (!isBotGroupAdmins) return client.reply(from, 'Perintah ini hanya bisa di gunakan ketika bot menjadi admin', id)
             const allMem = await client.getGroupMembers(groupId)
             for (let i = 0; i < allMem.length; i++) {
                 if (groupAdmins.includes(allMem[i].id)) {
